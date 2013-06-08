46186b1efcb73549cc56763a31646e4f73527029
przemek <przemyslaw.hejman@gmail.com>
2013-06-08 20:28:11 +0200

    Kolejne testy i testy

--- src/pl/pszt/fuzzylogic/FuzzyStatementTest.java
+++ src/pl/pszt/fuzzylogic/FuzzyStatementTest.java
@@ -16,6 +16,45 @@ public class FuzzyStatementTest extends TestCase{
         assertEquals(test1.getValue(), 0.9);
     }
 
+    public void testConstructor1() {
+        FuzzyStatement statement = new FuzzyStatement("test",-3.2);
+        assertEquals(statement.getValue(),0.0);
+    }
+
+    public void testConstructor2() {
+        FuzzyStatement statement = new FuzzyStatement("test",0);
+        assertEquals(statement.getValue(),0.0);
+    }
+
+    public void testConstructor3() {
+        FuzzyStatement statement = new FuzzyStatement("test",1.1);
+        assertEquals(statement.getValue(),1.0);
+    }
+
+    public void testSetter1 () {
+        FuzzyStatement statement = new FuzzyStatement("test",0.6);
+        statement.setValue(-3.2);
+        assertEquals(statement.getValue(),0.0);
+    }
+
+    public void testSetter2 () {
+        FuzzyStatement statement = new FuzzyStatement("test",0.6);
+        statement.setValue(6);
+        assertEquals(statement.getValue(),1.0);
+    }
+
+    public void testSetter3 () {
+        FuzzyStatement statement = new FuzzyStatement("test",0.6);
+        statement.setValue(0.3);
+        assertEquals(statement.getValue(),0.3);
+    }
+
+    public void testSetter4 () {
+        FuzzyStatement statement = new FuzzyStatement("test",0.6);
+        statement.setValue(0.99);
+        assertEquals(statement.getValue(),0.99);
+    }
+
     public void testNotOperator() {
         FuzzyStatement testStatement = new FuzzyStatement("Test",0.7);
         testStatement.negation();
@@ -26,13 +65,13 @@ public class FuzzyStatementTest extends TestCase{
         FuzzyStatement testStatement = new FuzzyStatement("Test",0.3);
         FuzzyStatement argument = new FuzzyStatement("Test",0.6);
         testStatement.conjunction(argument);
-        assertEquals(testStatement.getValue(),0.3);
+        assertEquals(testStatement.getValue(), 0.3);
     }
     public void testAndOperatorSymmetry() {
         FuzzyStatement testStatement = new FuzzyStatement("Test",0.3);
         FuzzyStatement argument = new FuzzyStatement("Test",0.6);
         argument.conjunction(testStatement);
-        assertEquals(testStatement.getValue(),0.3);
+        assertEquals(testStatement.getValue(), 0.3);
     }
 
     public void testOrOperator() {
